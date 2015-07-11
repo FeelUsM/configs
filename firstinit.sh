670d91559aa3448185786ffc17d961a2b4510117
FeelUs <fel1992@mail.ru>
2015-07-12 02:06:39 +0300

    настроил замену ссылок

--- firstinit.sh
+++ firstinit.sh
@@ -1,20 +1,16 @@
 #! /bin/bash
-if [ -f ~/.emacs -a ! -L ~/.emacs ]; then
-    mv ~/.emacs ~/.emacs.bak
-    ln -s ~/configs/_emacs.el ~/.emacs
-    echo ~/.emacs moved to ~/.emacs.bak
-else
-    echo ~/.emacs - not exists or is symbolic link
-fi
+linkto()
+{
+    #echo "$2" "$1"
+    if [ -f "$1" -o -L "$1" ] && [ -z "$(grep -P "\\Q$2\\E\$" <(ls -l "$1") )" ]; then
+	mv "$1" "$1".bak-"$(date "+%F-%H-%M-%S")"
+	echo "$!" moved to "$1".bak-"$(date "+%F-%H-%M-%S")"
+	ln -s "$2" "$1"
+    fi
+}
 
-if [ -f ~/.inputrc -a ! -L ~/.inputrc ]; then
-    mv ~/.inputrc ~/.inputrc.bak
-    ln -s ~/configs/_inutrc ~/.inputrc
-    echo ~/.input moved to ~/.inputrc.bak
-else
-    echo ~/.inputrc - not exists or is symbolic link
-fi
-
+linkto ~/.emacs ~/configs/_emacs.el
+linkto ~/.inputrc ~/configs/_inputrc
 
 [ -f ~/.bashrc ] && [ -z "$(grep 'source\ ~/configs/shellrc' ~/.bashrc)" ] && echo 'source\ ~/configs/shellrc' >> ~/.bashrc
 [ -f ~/.zshrc  ] && [ -z "$(grep 'source\ ~/configs/shellrc' ~/.zshrc )" ] && echo 'source\ ~/configs/shellrc' >> ~/.zshrc
