061d126bc6c42ecf19ceb6568bb894a73a71fc12
FeelUs <fel1992@mail.ru>
2016-07-26 06:22:42 +0300

    выравнивание, опечатка, комментарии

--- firstinit.sh
+++ firstinit.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
+
+# создает симлинк $1 указывающий на $2
+# если файл или симлинк с таким имененм существует - переименовывает его
 linkto()
 {
-    #echo "$2" "$1"
-    if [ -f "$1" -o -L "$1" ] && [ -z "$(grep -P "\\Q$2\\E\$" <(ls -l "$1") )" ]; then
-	mv "$1" "$1".bak-"$(date "+%F-%H-%M-%S")"
-	echo "$!" moved to "$1".bak-"$(date "+%F-%H-%M-%S")"
-    fi
-    [ -f "$1" -o -L "$1" ] || ln -s "$2" "$1"
+	#echo "$2" "$1"
+	if [ -f "$1" -o -L "$1" ] && [ -z "$(grep -P "\\Q$2\\E\$" <(ls -l "$1") )" ]; then
+		date_var="$(date "+%F-%H-%M-%S")"
+		mv "$1" "$1".bak-"$date_var"
+		echo "$1" moved to "$1".bak-"$date_var"
+	fi
+	# после переименования не должно остаться $1
+	[ -f "$1" -o -L "$1" ] || ln -s "$2" "$1"
 }
 
 linkto ~/.emacs ~/configs/_emacs.el
