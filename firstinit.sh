d26881c219d5c75edd62046b87f184e912f77300
FeelUs <fel1992@mail.ru>
2016-07-28 17:16:39 +0300

    добавил инициализацию git и перенес функции в scripts

--- firstinit.sh
+++ firstinit.sh
@@ -1,21 +1,17 @@
 #! /bin/bash
 
-# создает симлинк $1 указывающий на $2
-# если файл или симлинк с таким имененм существует - переименовывает его
-linkto()
-{
-	#echo "$2" "$1"
-	if [ -f "$1" -o -L "$1" ] && [ -z "$(grep -P "\\Q$2\\E\$" <(ls -l "$1") )" ]; then
-		date_var="$(date "+%F-%H-%M-%S")"
-		mv "$1" "$1".bak-"$date_var"
-		echo "$1" moved to "$1".bak-"$date_var"
-	fi
-	# после переименования не должно остаться $1
-	[ -f "$1" -o -L "$1" ] || ln -s "$2" "$1"
-}
+source ~/scripts/aliases-and-functions
+# for linkto() and append_line() 
 
 linkto ~/.emacs ~/configs/_emacs.el
 linkto ~/.inputrc ~/configs/_inputrc
 
-[ -f ~/.bashrc ] && [ -z "$(grep 'source\ ~/configs/shellrc' ~/.bashrc)" ] && echo 'source ~/configs/shellrc' >> ~/.bashrc || echo '~/.bashrc is epsent'
-[ -f ~/.zshrc  ] && [ -z "$(grep 'source\ ~/configs/shellrc' ~/.zshrc )" ] && echo 'source ~/configs/shellrc' >> ~/.zshrc || echo '~/.zshrc is epsent'
+[ -f ~/.bashrc ] && append_line 'source\ ~/configs/shellrc' ~/.bashrc || echo '~/.bashrc is epsent'
+[ -f ~/.zshrc  ] && append_line 'source\ ~/configs/shellrc' ~/.zshrc  || echo '~/.zshrc is epsent'
+
+if [ -n "$(git --version)" ]; then
+	git config --global user.name FeelUs
+	git config --global user.email 'fel1992@mail.ru'
+	# git config --global что-то там еще ...
+	git config --global core.quotepath false
+fi
